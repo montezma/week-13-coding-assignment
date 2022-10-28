97b8ec9070c3d2199f92e5dbb05f8c93031734c8
montezma <montez.noah@yahoo.com>
2022-10-27 23:10:58 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <div class='form-group'>
+          <h3 class="h333">LOG IN</h3> <br></br>
+          <input type="text" class="form-control" id="userName" placeholder="Enter UserName"></input> <br></br>
+          <input type="text" class="form-control" id="passWord" placeholder="Enter Password"></input> <br></br>
+          <input type="submit" value="Submit"></input> 
+          </div>
+        </form> 
+        <a><br></br>
+          Please update these class videos. 
+        </a> 
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
